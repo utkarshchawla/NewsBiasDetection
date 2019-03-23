e5e40ace31c9b14e809b3a1052762be5160265e6
utkarsh chawla <utkarshchawla691@gmail.com>
2019-03-24 00:13:44 +0530

    bug fixes

--- factual_titles.py
+++ factual_titles.py
@@ -1,25 +1,40 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import collections
+import pickle
+
+cat = "Sports"
+cats = {"National": 6, "Entertainment": 14, "Sports": 13}
 
 
 def get_titles():
     d = collections.defaultdict(list)
     page = ['']
-    for i in range(20):
+    for i in range(30):
         page.append(f'&page={i + 1}')
     for pageno in page:
-        url = f"http://ddnews.gov.in/about/news-archive?title=&news_type=6&changed_1=&changed_2={pageno}";
+        url = f"http://ddnews.gov.in/about/news-archive?title=&news_type={cats[cat]}&changed_1=&changed_2={pageno}";
         page = urlopen(url)
         soup = BeautifulSoup(page, 'html.parser')
         headline = soup.find_all('span', attrs={'class': 'field-content'})
 
         for item in headline:
-            date = item.find('p', attrs={'class': 'archive-date'}).get_text()
-            key = date.split("-")
-            key = key[2][:4] + "-" + key[1]
+            date = item.find('p', attrs={'class': 'archive-date'}).get_text().split("|")[0]
+            date = date[1:len(date) - 1]
             link = f"http://ddnews.gov.in{item.find('a').get('href')}"
             title = item.find('p', attrs={'class': 'archive-title'}).get_text()
-            d[key].append((title, link, date))
+            d[date].append((title, link))
 
     return d
+
+
+factual = get_titles()
+
+with open(f"factual-{cat}.pickle", "wb") as handle:
+    pickle.dump(factual, handle)
+
+total = 0
+for date in factual:
+    total += len(factual[date])
+print(total)
+print(factual)
--- ndtv_titles.py
+++ ndtv_titles.py
@@ -2,28 +2,83 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import collections
 import pickle
+import datetime
+import os
+
+cat = "Sports"
 
 ndtv = collections.defaultdict(list)
-with open("factual.pickle", "rb") as handle:
+with open(f"factual-{cat}.pickle", "rb") as handle:
     factual = pickle.load(handle)
 
-factual = list(factual.keys())
-
+dates = []
 for date in factual:
     date = date.split("-")
     date = date[2] + "-" + date[1]
+    dates.append(date)
+
+for date in set(dates):
     url = f"http://archives.ndtv.com/articles/{date}.html"
     date = ""
     page = urlopen(url)
     soup = BeautifulSoup(page, 'html.parser')
     headlines = soup.find("div", class_="related").find_all("a")
     for headline in headlines:
-        text = headline.get_text()
+        title = headline.get_text()
         link = headline.get('href')
         if link == 'javascript:void(0);':
-            date = text
+            date = title
         else:
-            ndtv[date].append(link)
+            ndtv[date].append((title, link))
 
-print(ndtv)
-print(len(ndtv))
+for date in list(ndtv.keys()):
+    new_date = datetime.datetime.strptime(date, '%d %B %Y').strftime('%d-%m-%Y')
+    ndtv[new_date] = ndtv.pop(date)
+
+
+def get_match(title, date):
+    hi = 0
+    rl = None
+    translationtable = str.maketrans("", "", "!@#$%^&*()-_+=|/<>.`~,:\"")
+    title = title.translate(translationtable).lower().split(" ")
+
+    prev = datetime.datetime.strptime(date, '%d-%m-%Y') - datetime.timedelta(1)
+    prev = prev.strftime('%d-%m-%Y')
+
+    next = datetime.datetime.strptime(date, '%d-%m-%Y') + datetime.timedelta(1)
+    next = next.strftime('%d-%m-%Y')
+    dlist = [date, next, prev]
+    for d in dlist:
+        list = ndtv[d]
+        for tup in list:
+            count = 0
+            test = tup[0].translate(translationtable).lower().split(" ")
+            for word in title:
+                if len(word) < 4: continue
+                if word in test:
+                    count += 1
+            if count > hi:
+                hi = count
+                rl = tup[1]
+    # return rl
+    if hi > 2:
+        return rl
+    else:
+        return None
+
+
+final_links = collections.defaultdict(list)
+for date in factual:
+    for tup in factual[date]:
+        ndtv_link = get_match(tup[0], date)
+        if ndtv_link is not None:
+            final_links[date].append((tup[1], ndtv_link))
+
+with open(f"final_links_{cat}.pickle", "wb") as handle:
+    pickle.dump(final_links, handle)
+print(final_links)
+int
+total = 0
+for date in final_links:
+    total += len(final_links[date])
+print(total)
